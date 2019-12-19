efd3545a6e1171c45907a8a27381751d3ce5972c
darsuabasi <uduakabasiabasiurua@pursuit.org>
2019-12-18 19:33:17 -0500

    logo, css, select box, button

---
+++ index.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let logo = document.querySelector("#imageLogo")
+    let selectBox = document.querySelector("#selectBox")
+    let mainData = document.querySelector("#mainData")
+})
+
+
+console.log("hello")
