8d419f45c5ebd4d52c19b0aa66e92bde10b2c7ee
darsuabasi <uduakabasiabasiurua@pursuit.org>
2019-12-20 00:36:07 -0500

    displaying title, des, year but undefined

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-    let header = document.querySelector("#header")
-    let selectBox = document.querySelector("#selectBox")
+    // let header = document.querySelector("#header")
+    let form = document.querySelector("form")
     let mainData = document.querySelector("#mainData")
-    let infoButton = document.querySelector("#infoButton")
     let userReviews = document.querySelector("#userReviews")
     let movieBox = document.querySelector("#movieBox")
     let formSubmission = document.querySelector("#formSubmission")
-    // let url = "https://ghibliapi.herokuapp.com/films"
-    let id ;
+    let reviewButton = document.querySelector("#reviewButton")
+    let id;
 
-    const displayMovies = async () => {
+    const populateMovies = async () => {
         try {
             let movies = await axios.get(`https://ghibliapi.herokuapp.com/films`)
             let movie = movies.data
@@ -17,71 +16,54 @@ document.addEventListener("DOMContentLoaded", () => {
             movie.forEach(el => {
                 let option = document.createElement("option")
                 option.innerText = el.title;
-                option.value = el.url          // for each value, grab the url; make it the innerText
+                
                 movieBox.appendChild(option)
+
+
+
             })
-        }catch (err) {
+        } catch (err) {
             console.log(err)
         }
     }
-   displayMovies()
-    const getReviews = async () => {
+    populateMovies()
+
+    const displayReviews = async () => {
         try {
-            let allFilms = await axios.get("https://ghibliapi.herokuapp.com/films")
-            let filmInfo = allFilms.data
-            console.log(filmInfo)
-            // allFilms.forEach(el => {
-            //     let movieTitle = filmInfo.title;
-            //     let releaseYear = filmInfo.release_date;
-            //     let movieDescription = filmInfo.description;
+            let allFilms = await axios.get("https://ghibliapi.herokuapp.com/films/")
+            let film = allFilms.data;
+            let movieTitle = film.title
+            let releaseYear = film.release_date
+            let movieDescription = film.description
 
-            //     h3.innerText = movieTitle;
-            //     let h3 = document.createElement("h3")
-            //     mainData.appendChild(h3)
+            let h3 = document.createElement("h3")
+            h3.innerText = movieTitle
+            mainData.appendChild(h3)
 
-            //     p1.innerText = releaseYear;
-            //     let p1 = document.createElement("p")
-            //     mainData.appendChild(p1)
 
-            //     p2.innerText = movieDescription;
-            //     let p2 = document.createElement("p")
-            //     mainData.appendChild(p2)
 
-            // })
-            title.innerText = filmInfo.title
-            releaseYear.innerText = filmInfo.releaseYear
-            movieDescription.innerText = filmInfo.movieDescription
+            let p1 = document.createElement("p")
+            p1.innerText = releaseYear;
+            mainData.appendChild(p1)
+
+
+            let p2 = document.createElement("p")
+            p2.innerText = movieDescription;
+            mainData.appendChild(p2)
+
+
 
         } catch (err) {
             console.log(err)
         }
     }
-    infoButton.addEventListener("click", () => {
+
+
+
+    movieBox.addEventListener("change", (event) => {
         document.getElementById("mainData").innerHTML = " ";
-        getReviews()
-    })
-    formSubmission.addEventListener("submit",(event) =>{
-        event.preventDefault()
-        let newLi = document.createElement("li")
-        newLi.innerText = "userInput.value"
-        userReviews.appendChild(newLi)
-    
-    
-    })
 
+        displayReviews(event.currentTarget.value)
+
+    })
 })
-
-// dropdown box ; when you change the drop down slector 
-
-// select.addEventListener("change", (event) => {
-//     title.innerText = ""
-//     releaseEvents.innerText = ""
-//     description.innerText = ""
-//     getReviews(event.currentTarget.value)
-// })
-
-
-
-
-
-
